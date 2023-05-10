882ee83400a35a7c6f57d2d85958be910ab7b184
BurhanSoy <burhansoy1@gmail.com>
2023-05-10 16:30:05 +0300

    Comment class added.

---
+++ LineageBlog.Entities/Concrete/Comment.cs
@@ -0,0 +1,16 @@
+﻿using LineageBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineageBlog.Entities.Concrete
+{
+    public class Comment : EntityBase, IEntity
+    {
+        public string Text { get; set; }
+        public int ArticleId { get; set; }
+        public Article Article { get; set; } //Yorum yapan kişinin ad ve tarih bilgisini base class'tan alabiliriz.
+    }
+}
