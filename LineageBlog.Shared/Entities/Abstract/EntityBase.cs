5889a10ee0dea1c50a9ed025a0fa183bf4460452
BurhanSoy <burhansoy1@gmail.com>
2023-05-10 15:53:33 +0300

    Add project files.

---
+++ LineageBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineageBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        //Verdiğimiz base değerlerin diğer sınıflarda değişikliğe (override) uğramasını isteyebileceğimiz için abstract
+
+        public virtual int Id { get; set; }
+        public virtual DateTime CretedDate { get; set; } = DateTime.Now; //Bu değerlerin override edilmesini istiyorsak virtual olmak zorunda
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false; // Bu makale ya da kullanıcı silindi mi?
+        public virtual bool IsActive { get; set; } = true; //Bu makale ya da bu kullanıcı aktif mi?
+        public virtual string CreatedByName { get; set; } // Blogda kayıt ol kısmı olmayacağı için üye olmayan kullanıcı beğeni veya yorum atması için string
+        public virtual string ModifiedByName { get; set; }
+    }
+}
---
+++ LineageBlog.Shared/Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineageBlog.Shared.Entities.Abstract
+{
+    public interface IEntity
+    {
+        //repository pattern içindeki entitylerin IEntity olmasını istediğimiz için şimdilik bir ekleme yapmayacağım.
+    }
+}
