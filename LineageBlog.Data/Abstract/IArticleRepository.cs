0231e64e3fe1fd3fd732ed8077dc203d44463d88
BurhanSoy <burhansoy1@gmail.com>
2023-05-11 17:40:03 +0300

    Implemented IEntityRepository in the data layer.

---
+++ LineageBlog.Data/Abstract/IArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using LineageBlog.Entities.Concrete;
+using LineageBlog.Shared.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineageBlog.Data.Abstract
+{
+    public interface IArticleRepository:IEntityRepository<Article>
+    {
+    }
+}
