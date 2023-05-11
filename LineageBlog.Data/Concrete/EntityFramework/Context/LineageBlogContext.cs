9ab9b20e2f7a64ade796f6baabd775584600815a
BurhanSoy <burhansoy1@gmail.com>
2023-05-11 18:27:33 +0300

    DbContext object created.

---
+++ LineageBlog.Data/Concrete/EntityFramework/Context/LineageBlogContext.cs
@@ -0,0 +1,25 @@
+﻿using LineageBlog.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineageBlog.Data.Concrete.EntityFramework.Context
+{
+    public class LineageBlogContext:DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public LineageBlogContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
--- LineageBlog.Mvc/Program.cs
+++ LineageBlog.Mvc/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("myconn")));
 
 var app = builder.Build();
 
